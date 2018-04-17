5c7e372faebd310a8c7a86431448af61b73147ff
Jinx <chisel.delahanty@gmail.com>
2018-04-17 22:57:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// user starts typing
+// text entered is compared against items saved
+// user sees suggested results: how?
+// option 1: they come up in a "live dropdown"
+// option 2: they appear clickable under RESULTS
+// branch 1: user sees the correct item
+// b1: user selects correct item
+// b1: on click, RESULTS area loads item info
+// b1: also on click, RESULTS bkgd color changes
+// branch 2: user does not see correct item
+// b2: user clicks the word "here"
+// b2: user is taken to submission page
+// b2: user enters name of item & category
+// b2: item is saved in database
+// b2: user sees "Thanks!" message
+// b2: user is returned to start page
