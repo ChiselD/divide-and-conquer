8f4f479fb8e2552c63ffbf8ad4cbe517634d3191
Jinx <chisel.delahanty@gmail.com>
2018-04-26 21:51:22 +0200

    finish selection-recognition function and comment it

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+
+// function that makes your selected type of trash appear below the input
 function onInput() {
+  // select the text input
   var val = document.getElementById("trashytrash").value;
+  // select all the items on the datalist
   var opts = document.getElementById("trash").childNodes;
+  // constantly compare content of input to every item on datalist
   for (var i = 0; i < opts.length; i++) {
     if (opts[i].value === val) {
-      // DO SOMETHING
-      alert(opts[i].value);
+      // select destination spot
+      var destination = document.getElementById("results-here");
+      // clear any previous content in destination spot
+      while (destination.firstChild) {
+        destination.removeChild(destination.firstChild);
+      }
+      // insert newly selected type of trash into results window
+      var content = document.createTextNode(opts[i].value);
+      destination.appendChild(content);
       break;
     }
   }
